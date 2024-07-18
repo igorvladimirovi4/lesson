b26142565aae3b919740f0319f35a52d4b961417
igorvladimirovi4 <115870308+igorvladimirovi4@users.noreply.github.com>
2024-07-18 17:26:30 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+immutable_var= 1, 'two', 3, 'four'
+print(immutable_var)
+
+immutable_var[0]= 100
+print(immutable_var)
+# обращение по элементам кортеж не поддерживает
+
+mutable_list= (['one', 'two'], 3, 4)
+mutable_list[0][0]= 1
+print(mutable_list)
